4f5f2d0afb4ce5d46821b8561f5e422fd679ea65
Tiberiu Tesileanu <ttesileanu@flatironinstitute.org>
2023-04-07 10:49:31 -0600

    Add data-driven controller to linear sys nb script

--- sandbox/experiment_linear_system.py
+++ sandbox/experiment_linear_system.py
@@ -551,4 +551,646 @@ with dv.FigureManager(figsize=(4, 4)) as (_, ax):
     ax.set_ylim([-1.1, 1.1])
     ax.set_aspect(1)
 
+# %% [markdown]
+# ## A data-driven controller
+
+
+# %%
+class DDController:
+    def __init__(
+        self,
+        observation_dim: int,
+        control_dim: int,
+        history_length: int,
+        seed_length: int = 1,
+        control_horizon: int = 1,
+        observation_match_cost: float = 1.0,
+        control_match_cost: float = 1.0,
+        target_cost: float = 0.01,
+        control_cost: float = 0.01,
+        control_sparsity: float = 0.0,
+    ):
+        """Data-drive controller.
+
+        This acts by leveraging the behavioral approach to dynamical systems: a system
+        is identified by the fact that its possible trajectories are restricted to a
+        certain subspace. Observing actual trajectories can then be used to identify
+        this subspace and, thus, the system.
+
+        Given a matrix `Y` in which each column is a sequence of observations of a
+        dynamical system and a matrix `U` in which each column is a sequence of control
+        inputs to the system, and assuming that the number of columns is large enough,
+        any new sequence of controls `u` and observations `y` can be written as a linear
+        combination of the earlier data:
+
+            z = Z alpha ,
+
+        where `Z = vstack((Y, U))`, `z = vstack((y, u))`, and `alpha` is a vector of
+        coefficients. The matrices `Y` and `U` can be obtained from a single long
+        sequence in the form of Hankel matrices -- and that is what we will be doing
+        here.
+
+        In our case, we want to establish a control plan that maximally reduces the
+        magnitude of the observations after a certain horizon, using the earlier
+        measurements to predict the behavior of the system. To perform this prediction
+        we need to start with a "seed" where the observations and control are known --
+        the seed should be sufficiently long to fully specify the (unobserved) internal
+        state of the system.
+
+        More specifically, suppose the system's state is fully identified by observing
+        `seed_length` steps and suppose that we're trying to minimize the system's
+        output a number `control_horizon` of steps in the future. Since we want to make
+        sure that the full (unobserved) state of the system is under control, we also
+        evaluate the target output by summing over `seed_length` steps. The number of
+        prior samples used in the prediction is given by `history_length`.
+
+        The control planning is set up as an optimization problem where the coefficients
+        `alpha` are inferred such that a loss function containing five terms is
+        minimized:
+
+            L = (observation_match_cost * (predicted - measured seed observations)) ** 2
+              + (control_match_cost * (predicted - measured seed controls)) ** 2
+              + (target_cost * (predicted observations at horizon)) ** 2
+              + (control_cost * (predicted control after seed)) ** 2
+              + control_sparsity * L1_norm(predicted control after seed) .
+
+        The inferred coefficients are then used to predict the optimal control. Note
+        that apart from the L1 regularization term, this is just a least-squares
+        problem.
+
+        :param observation_dim: dimensionality of measurements
+        :param control_dim: dimensionality of control
+        :param history_length: number of columns to use in the Hankel matrices
+        :param seed_length: number of measurements needed to fully specify an internal
+            state
+        :param control_horizon: how far ahead to aim for reduced measurements
+        :param observation_match_cost: multiplier for matching observation values
+        :param control_match_cost: multiplier for matching seed control values
+        :param target_cost: multiplier for minimizing observation values at horizon
+        :param control_cost: multiplier for minimizing overall control magnitude
+        :param control_sparsity: amount of sparsity-inducing, L1 regularizer; NOT YET
+            IMPLEMENTED
+        """
+        self.observation_dim = observation_dim
+        self.control_dim = control_dim
+        self.history_length = history_length
+        self.seed_length = seed_length
+        self.control_horizon = control_horizon
+        self.observation_match_cost = observation_match_cost
+        self.control_match_cost = control_match_cost
+        self.target_cost = target_cost
+        self.control_cost = control_cost
+        self.control_sparsity = control_sparsity
+        if self.control_sparsity != 0:
+            assert NotImplementedError("control_sparsity not implemented yet")
+
+        self.observation_history = None
+        self.control_history = None
+
+    def feed(self, observation: torch.Tensor, control: torch.Tensor):
+        """Register another measurement.
+
+        At most `self.history_length` samples are kept.
+
+        :param observation: output from the model
+        :param control: control input
+        """
+        assert len(observation) == self.observation_dim
+        assert len(control) == self.control_dim
+
+        if self.observation_history is None:
+            self.observation_history = observation.clone()
+        else:
+            self.observation_history = torch.vstack(
+                (self.observation_history, observation)
+            )
+        if self.control_history is None:
+            self.control_history = control.clone()
+        else:
+            self.control_history = torch.vstack((self.control_history, control))
+
+        n = self.history_length + self.control_horizon + self.seed_length
+        self.observation_history = self.observation_history[-n:]
+        self.control_history = self.control_history[-n:]
+
+    def plan(self) -> torch.Tensor:
+        """Estimate optimal control plan given the current history.
+
+        :return: control plan, shape `(control_horizon, control_dim)`
+        """
+        p = self.seed_length
+        l = self.control_horizon
+        n = self.history_length
+
+        d = self.observation_dim
+        c = self.control_dim
+
+        if len(self.observation_history) < n + p + l:
+            # not enough data yet
+            return torch.zeros((l, c))
+
+        obs = self.observation_history
+        ctrl = self.control_history
+
+        end = len(obs)
+        assert len(ctrl) == end
+
+        Y = torch.empty((2 * p * d, n))
+        U = torch.empty(((p + l) * c, n))
+        # target y and target z are zero
+        y = torch.zeros((2 * p * d, 1))
+        u = torch.zeros((p + l) * c, 1)
+        for i in range(p):
+            yi = i * d
+            Y[yi : yi + d] = obs[i : i + n].T
+            y[yi : yi + d] = obs[end + i - p][:, None]
+
+            yi = (i + p) * d
+            Y[yi : yi + d] = obs[l + i : l + i + n].T
+
+            ui = i * c
+            U[ui : ui + c] = ctrl[i : i + n].T
+            u[ui : ui + c] = ctrl[end + i - p][:, None]
+
+        for i in range(p, p + l):
+            ui = i * c
+            U[ui : ui + c] = ctrl[i : i + n].T
+
+        Z = torch.vstack((Y, U))
+        z = torch.vstack((y, u))
+
+        # weigh using the appropriate coefficients
+        Z_weighted = Z.clone()
+        Z_weighted[: p * d] *= self.observation_match_cost
+        Z_weighted[p * d : 2 * p * d] *= self.target_cost
+        Z_weighted[2 * p * d : p * (2 * d + c)] *= self.control_match_cost
+        Z_weighted[-l * c :] *= self.control_cost
+
+        z_weighted = z.clone()
+        z_weighted[: p * d] *= self.observation_match_cost
+        # nothing to multiply for [p * d : 2 * p * d] as target y is zero
+
+        z_weighted[2 * p * d : p * (2 * d + c)] *= self.control_match_cost
+        # nothing to multiply for [-l * c :] as target u is zero
+
+        # now solve z = Z alpha
+        result = torch.linalg.lstsq(Z_weighted, z_weighted)
+        z_hat = Z @ result.solution
+
+        return z_hat[-l * c :].reshape((l, c))
+
+
+# %% [markdown]
+# ### Try the DD controller
+# #### A 1d example
+
+# %%
+torch.manual_seed(42)
+model = LinearSystem(
+    evolution=torch.tensor([[1.1]]),
+    control=torch.tensor([[1.0]]),
+    initial_state=torch.tensor([[1.0]]),
+)
+history_length = 25
+control_horizon = 5
+controller = DDController(
+    1,
+    1,
+    history_length,
+    control_horizon=control_horizon,
+    control_cost=0,
+    target_cost=0.1,
+)
+
+n_steps = 100
+control = torch.tensor([0.0])
+control_noise = 0.001
+outputs = []
+controls = []
+for k in range(n_steps):
+    # need noise for exploration
+    control = control + control_noise * torch.randn(control.shape)
+    y = model.run(control_plan=control[None, :])
+    y = y[0, :, 0]
+
+    outputs.append(y)
+    controls.append(control)
+
+    controller.feed(y, control)
+    control_plan = controller.plan()
+    control = control_plan[0]
+
+control_start = (
+    controller.history_length + controller.control_horizon + controller.seed_length
+)
+
+outputs = torch.stack(outputs)
+controls = torch.stack(controls)
+
+# %%
+with dv.FigureManager(2, 1, figsize=(6, 4)) as (_, axs):
+    yl = (outputs.min(), outputs.max())
+    axs[0].fill_betweenx(
+        yl,
+        [0, 0],
+        2 * [control_start],
+        color="gray",
+        alpha=0.5,
+        edgecolor="none",
+        label="no control",
+    )
+    axs[0].plot(outputs.squeeze())
+    axs[0].set_xlabel("time")
+    axs[0].set_ylabel("observation")
+    axs[0].legend(frameon=False)
+
+    yl = (controls.min(), controls.max())
+    axs[1].fill_betweenx(
+        yl,
+        [0, 0],
+        2 * [control_start],
+        color="gray",
+        alpha=0.5,
+        edgecolor="none",
+        label="no control",
+    )
+    axs[1].plot(controls.squeeze())
+    axs[1].set_xlabel("time")
+    axs[1].set_ylabel("control")
+    axs[1].legend(frameon=False, loc="lower right")
+
+# %% [markdown]
+# #### A 2d example
+
+# %%
+torch.manual_seed(42)
+model = LinearSystem(
+    evolution=torch.tensor([[1.1, 0.0], [0.0, 0.8]]),
+    control=torch.tensor([[1.0], [-0.3]]),
+    initial_state=torch.tensor([[1.0], [0.5]]),
+)
+history_length = 25
+control_horizon = 5
+controller = DDController(
+    2,
+    1,
+    history_length,
+    control_horizon=control_horizon,
+    control_cost=0,
+    target_cost=0.1,
+)
+
+n_steps = 200
+control = torch.tensor([0.0])
+control_noise = 0.001
+outputs = []
+controls = []
+for k in range(n_steps):
+    # need noise for exploration
+    control = control + control_noise * torch.randn(control.shape)
+    y = model.run(control_plan=control[None, :])
+    y = y[0, :, 0]
+
+    outputs.append(y)
+    controls.append(control)
+
+    controller.feed(y, control)
+    control_plan = controller.plan()
+    control = control_plan[0]
+
+control_start = (
+    controller.history_length + controller.control_horizon + controller.seed_length
+)
+
+outputs = torch.stack(outputs)
+controls = torch.stack(controls)
+
+# %%
+with dv.FigureManager(3, 1, figsize=(6, 6)) as (_, axs):
+    for i in range(2):
+        ax = axs[i]
+        output = outputs[:, i]
+        yl = (output.min(), output.max())
+        ax.fill_betweenx(
+            yl,
+            [0, 0],
+            2 * [control_start],
+            color="gray",
+            alpha=0.5,
+            edgecolor="none",
+            label="no control",
+        )
+        ax.plot(output.squeeze())
+        ax.set_xlabel("time")
+        ax.set_ylabel(f"observation $y_{{{i + 1}}}$")
+        ax.legend(frameon=False)
+
+    yl = (controls.min(), controls.max())
+    axs[2].fill_betweenx(
+        yl,
+        [0, 0],
+        2 * [control_start],
+        color="gray",
+        alpha=0.5,
+        edgecolor="none",
+        label="no control",
+    )
+    axs[2].plot(controls.squeeze())
+    axs[2].set_xlabel("time")
+    axs[2].set_ylabel("control")
+    axs[2].legend(frameon=False, loc="lower right")
+
+# %% [markdown]
+# #### 2d with rotation and observation noise
+
+# %%
+torch.manual_seed(42)
+rot_mat = torch.FloatTensor([[np.cos(0.1), np.sin(0.1)], [-np.sin(0.1), np.cos(0.1)]])
+scale_mat = torch.FloatTensor([[1.1, 0.0], [0.0, 0.8]])
+transfer_mat = scale_mat @ rot_mat
+model = LinearSystem(
+    evolution=transfer_mat,
+    control=torch.tensor([[1.0], [-0.3]]),
+    observation_noise=torch.tensor([[0.1, 0.0], [0.0, 0.2]]),
+    initial_state=torch.tensor([[1.0], [0.5]]),
+)
+history_length = 25
+control_horizon = 5
+controller = DDController(
+    2,
+    1,
+    history_length,
+    control_horizon=control_horizon,
+    control_cost=0,
+    target_cost=0.1,
+)
+
+n_steps = 200
+control = torch.tensor([0.0])
+control_noise = 0.001
+outputs = []
+controls = []
+for k in range(n_steps):
+    # need noise for exploration
+    control = control + control_noise * torch.randn(control.shape)
+    y = model.run(control_plan=control[None, :])
+    y = y[0, :, 0]
+
+    outputs.append(y)
+    controls.append(control)
+
+    controller.feed(y, control)
+    control_plan = controller.plan()
+    control = control_plan[0]
+
+control_start = (
+    controller.history_length + controller.control_horizon + controller.seed_length
+)
+
+outputs = torch.stack(outputs)
+controls = torch.stack(controls)
+
+# %%
+with dv.FigureManager(3, 1, figsize=(6, 6)) as (_, axs):
+    for i in range(2):
+        ax = axs[i]
+        output = outputs[:, i]
+        yl = (output.min(), output.max())
+        ax.fill_betweenx(
+            yl,
+            [0, 0],
+            2 * [control_start],
+            color="gray",
+            alpha=0.5,
+            edgecolor="none",
+            label="no control",
+        )
+        ax.plot(output.squeeze())
+        ax.set_xlabel("time")
+        ax.set_ylabel(f"observation $y_{{{i + 1}}}$")
+        ax.legend(frameon=False)
+
+    yl = (controls.min(), controls.max())
+    axs[2].fill_betweenx(
+        yl,
+        [0, 0],
+        2 * [control_start],
+        color="gray",
+        alpha=0.5,
+        edgecolor="none",
+        label="no control",
+    )
+    axs[2].plot(controls.squeeze())
+    axs[2].set_xlabel("time")
+    axs[2].set_ylabel("control")
+    axs[2].legend(frameon=False, loc="lower right")
+
+# %%
+with dv.FigureManager() as (_, ax):
+    h = dv.color_plot(
+        outputs[:, 0].numpy(),
+        outputs[:, 1].numpy(),
+        np.arange(len(outputs)),
+        cmap="viridis",
+        ax=ax,
+    )
+    cb = dv.colorbar(h)
+    cb.set_label("time")
+    ax.autoscale()
+    ax.set_xlabel("$y_1$")
+    ax.set_ylabel("$y_2$")
+
+    ax.set_xlim(-5, 5)
+    ax.set_ylim(-5, 5)
+
+# %% [markdown]
+# #### 2d with rotation and state noise
+
+# %%
+torch.manual_seed(42)
+rot_mat = torch.FloatTensor([[np.cos(0.1), np.sin(0.1)], [-np.sin(0.1), np.cos(0.1)]])
+scale_mat = torch.FloatTensor([[1.1, 0.0], [0.0, 0.8]])
+transfer_mat = scale_mat @ rot_mat
+model = LinearSystem(
+    evolution=transfer_mat,
+    control=torch.tensor([[1.0], [-0.3]]),
+    state_noise=torch.tensor([[0.05, 0.0], [0.0, 0.1]]),
+    initial_state=torch.tensor([[1.0], [0.5]]),
+)
+history_length = 25
+control_horizon = 5
+controller = DDController(
+    2,
+    1,
+    history_length,
+    control_horizon=control_horizon,
+    control_cost=0,
+    target_cost=0.1,
+)
+
+n_steps = 200
+control = torch.tensor([0.0])
+control_noise = 0.001
+outputs = []
+controls = []
+for k in range(n_steps):
+    # need noise for exploration
+    control = control + control_noise * torch.randn(control.shape)
+    y = model.run(control_plan=control[None, :])
+    y = y[0, :, 0]
+
+    outputs.append(y)
+    controls.append(control)
+
+    controller.feed(y, control)
+    control_plan = controller.plan()
+    control = control_plan[0]
+
+control_start = (
+    controller.history_length + controller.control_horizon + controller.seed_length
+)
+
+outputs = torch.stack(outputs)
+controls = torch.stack(controls)
+
+# %%
+with dv.FigureManager(3, 1, figsize=(6, 6)) as (_, axs):
+    for i in range(2):
+        ax = axs[i]
+        output = outputs[:, i]
+        yl = (output.min(), output.max())
+        ax.fill_betweenx(
+            yl,
+            [0, 0],
+            2 * [control_start],
+            color="gray",
+            alpha=0.5,
+            edgecolor="none",
+            label="no control",
+        )
+        ax.plot(output.squeeze())
+        ax.set_xlabel("time")
+        ax.set_ylabel(f"observation $y_{{{i + 1}}}$")
+        ax.legend(frameon=False)
+
+    yl = (controls.min(), controls.max())
+    axs[2].fill_betweenx(
+        yl,
+        [0, 0],
+        2 * [control_start],
+        color="gray",
+        alpha=0.5,
+        edgecolor="none",
+        label="no control",
+    )
+    axs[2].plot(controls.squeeze())
+    axs[2].set_xlabel("time")
+    axs[2].set_ylabel("control")
+    axs[2].legend(frameon=False, loc="lower right")
+
+# %%
+with dv.FigureManager() as (_, ax):
+    h = dv.color_plot(
+        outputs[:, 0].numpy(),
+        outputs[:, 1].numpy(),
+        np.arange(len(outputs)),
+        cmap="viridis",
+        ax=ax,
+    )
+    cb = dv.colorbar(h)
+    cb.set_label("time")
+    ax.autoscale()
+    ax.set_xlabel("$y_1$")
+    ax.set_ylabel("$y_2$")
+
+    ax.set_xlim(-10, 10)
+    ax.set_ylim(-10, 10)
+
+
+# %% [markdown]
+# #### 2d rotation, state noise, and partial observation
+
+# %%
+torch.manual_seed(42)
+rot_mat = torch.FloatTensor([[np.cos(0.1), np.sin(0.1)], [-np.sin(0.1), np.cos(0.1)]])
+scale_mat = torch.FloatTensor([[1.1, 0.0], [0.0, 0.8]])
+transfer_mat = scale_mat @ rot_mat
+model = LinearSystem(
+    evolution=transfer_mat,
+    observation=torch.tensor([[1.0, 0.0]]),
+    control=torch.tensor([[1.0], [-0.3]]),
+    state_noise=torch.tensor([[0.05, 0.0], [0.0, 0.1]]),
+    initial_state=torch.tensor([[1.0], [0.5]]),
+)
+history_length = 25
+control_horizon = 8
+controller = DDController(
+    1,
+    1,
+    history_length,
+    seed_length=2,
+    control_horizon=control_horizon,
+    control_cost=0,
+    target_cost=0.1,
+)
+
+n_steps = 200
+control = torch.tensor([0.0])
+control_noise = 0.001
+outputs = []
+controls = []
+for k in range(n_steps):
+    # need noise for exploration
+    control = control + control_noise * torch.randn(control.shape)
+    y = model.run(control_plan=control[None, :])
+    y = y[0, :, 0]
+
+    outputs.append(y)
+    controls.append(control)
+
+    controller.feed(y, control)
+    control_plan = controller.plan()
+    control = control_plan[0]
+
+control_start = (
+    controller.history_length + controller.control_horizon + controller.seed_length
+)
+
+outputs = torch.stack(outputs)
+controls = torch.stack(controls)
+
+# %%
+with dv.FigureManager(2, 1, figsize=(6, 4)) as (_, axs):
+    ax = axs[0]
+    output = outputs[:, 0]
+    yl = (output.min(), output.max())
+    ax.fill_betweenx(
+        yl,
+        [0, 0],
+        2 * [control_start],
+        color="gray",
+        alpha=0.5,
+        edgecolor="none",
+        label="no control",
+    )
+    ax.plot(output.squeeze())
+    ax.set_xlabel("time")
+    ax.set_ylabel(f"observation $y_{{{i + 1}}}$")
+    ax.legend(frameon=False)
+
+    yl = (controls.min(), controls.max())
+    axs[1].fill_betweenx(
+        yl,
+        [0, 0],
+        2 * [control_start],
+        color="gray",
+        alpha=0.5,
+        edgecolor="none",
+        label="no control",
+    )
+    axs[1].plot(controls.squeeze())
+    axs[1].set_xlabel("time")
+    axs[1].set_ylabel("control")
+    axs[1].legend(frameon=False, loc="lower right")
+
 # %%
