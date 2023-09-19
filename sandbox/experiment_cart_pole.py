80be7d2994f8f303169a8490f0fd5f027464c61a
Tiberiu Tesileanu <ttesileanu@flatironinstitute.org>
2023-09-19 11:43:21 -0400

    Convert cart pole and double cart pole to new code

--- sandbox/experiment_cart_pole.py
+++ sandbox/experiment_cart_pole.py
@@ -16,20 +16,22 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pydove as dv
 
-from ddc import DDController
+from ddc import GeneralSystem, DeepControl
 
 # %% [markdown]
 # ## Define the simulator
 
 
-class CartPole:
+class CartPole(GeneralSystem):
     def __init__(
         self,
+        dt: float,
         M: float = 1.0,
         m: float = 0.1,
         l: float = 0.5,
         g: float = 9.8,
-        dt: float = 0.02,
+        observation=None,
+        **kwargs,
     ):
         # parameters
         self.M = M
@@ -38,87 +40,52 @@ class CartPole:
         self.g = g
         self.dt = dt
 
-        # current state: positions and velocities
-        self.q = torch.zeros(2)  # [x, theta]
-        self.dq = torch.zeros(2)
+        super().__init__(
+            self.evolution_fct, observation=observation, state_dim=4, **kwargs
+        )
 
-    def step(self, f: float):
+    def evolution_fct(self, state: torch.Tensor, force: torch.Tensor):
         """Apply the given force to the cart and update the state by one time step."""
-        theta = self.q[1]
-        c = torch.cos(theta)
-        s = torch.sin(theta)
-        denom = self.M + self.m * s**2
-
-        ml = self.m * self.l
-        mg = self.m * self.g
-        dtheta = self.dq[1]
-        ddx = (f + ml * dtheta**2 * s - mg * s * c) / denom
-
-        Mtotg = (self.M + self.m) * self.g
-        ddtheta = (Mtotg * s - f * c - ml * dtheta**2 * s * c) / denom / self.l
-
-        # ddq = torch.tensor([ddx, ddtheta]).to(dtype=self.q.dtype)
-
-        # leap frog
-        # change = (0.5 * self.dt) * ddq
-        # self.dq += change
-        # self.q += self.dt * self.dq
-        # self.dq += change
-
-        # Euler
-        # change = self.dt * ddq
-        # self.q += self.dt * self.dq
-        # self.dq += change
-
         # RK4
         h = self.dt
-        y = torch.hstack((self.q, self.dq))
-        k1 = self._gradient(y, f)
-        k2 = self._gradient(y + (0.5 * h) * k1, f)
-        k3 = self._gradient(y + (0.5 * h) * k2, f)
-        k4 = self._gradient(y + h * k3, f)
+        k1 = self._gradient(state, force)
+        k2 = self._gradient(state + (0.5 * h) * k1, force)
+        k3 = self._gradient(state + (0.5 * h) * k2, force)
+        k4 = self._gradient(state + h * k3, force)
 
-        dy = (h / 6) * (k1 + 2 * k2 + 2 * k3 + k4)
-        self.q += dy[:2]
-        self.dq += dy[2:]
+        dstate = (h / 6) * (k1 + 2 * k2 + 2 * k3 + k4)
 
-    def _gradient(self, y: torch.Tensor, f: float) -> torch.Tensor:
-        theta = y[1]
+        return state + dstate
+
+    def _gradient(self, state: torch.Tensor, force: float) -> torch.Tensor:
+        x, theta, dx, dtheta = state
         c = torch.cos(theta)
         s = torch.sin(theta)
         denom = self.M + self.m * s**2
 
         ml = self.m * self.l
         mg = self.m * self.g
-        dtheta = y[3]
-        ddx = (f + ml * dtheta**2 * s - mg * s * c) / denom
+        ddx = (force + ml * dtheta**2 * s - mg * s * c) / denom
 
         Mtotg = (self.M + self.m) * self.g
-        ddtheta = (Mtotg * s - f * c - ml * dtheta**2 * s * c) / denom / self.l
+        ddtheta = (Mtotg * s - force * c - ml * dtheta**2 * s * c) / denom / self.l
 
-        grad = torch.tensor([y[2], y[3], ddx, ddtheta]).to(dtype=y.dtype)
+        grad = torch.tensor([[dx], [dtheta], [ddx], [ddtheta]]).to(dtype=state.dtype)
         return grad
-
-    def astype(self, t: torch.dtype) -> "CartPole":
-        self.q.to(dtype=t)
-        self.dq.to(dtype=t)
-        return self
 
 
 # %% [markdown]
 # ### Test the simulation of the cart pole
 
 # %%
-pendulum = CartPole()
-pendulum.q[1] = 1e-6
+pendulum = CartPole(dt=0.02, initial_state=torch.tensor([[0], [1e-6], [0], [0]]))
 
 n_steps = 1000
 phase = torch.zeros((4, n_steps))
 
 for i in range(n_steps):
-    phase[:2, i] = pendulum.q
-    phase[2:, i] = pendulum.dq
-    pendulum.step(0.0)
+    phase[:, i] = pendulum.state.squeeze()
+    pendulum.run(1)
 
 # %%
 with dv.FigureManager(2, 1, figsize=(4, 4), constrained_layout=True) as (_, axs):
@@ -132,40 +99,39 @@ with dv.FigureManager(2, 1, figsize=(4, 4), constrained_layout=True) as (_, axs)
 # %%
 torch.manual_seed(42)
 control_horizon = 12
-controller = DDController(
-    2,
-    1,
-    seed_length=2,
-    averaging_factor=2.0,
-    control_horizon=control_horizon,
-    noise_handling="none",
-    l2_regularization=5.0,
-    noise_strength=0.01,
-    output_cost=100.0,
+controller = DeepControl(
+    control_dim=1,
+    ini_length=2,
+    horizon=control_horizon,
+    l2_regularization=1e-4,
+    control_cost=1e-5,
+    seed_noise_norm=2.0,
     affine=True,
-    # clip_control=(-40, 40),
-    clip_control=(-1, 1),
+    control_norm_clip=5.0,
 )
 
-pendulum = CartPole(M=100)
-
-# initial state
-pendulum.q[1] = np.pi - 0.05
-# pendulum.q[1] = 1e-6
+model = CartPole(
+    dt=0.02,
+    M=100,
+    observation=lambda state: state[[1, 2]],
+    initial_state=torch.tensor([[0], [np.pi - 0.05], [0], [0]], dtype=torch.float64),
+)
 
 n_steps = 2000
-for k in range(n_steps):
-    # decide on control magnitude
-    obs = torch.hstack((pendulum.q[[1]], pendulum.dq[[0]]))
-    controller.feed(obs)
+planning_steps = 2
+seed_length = 300
+
+control_snippet = controller.generate_seed(seed_length, model.dtype)
+control_start = len(control_snippet)
+for k in range(n_steps // planning_steps):
+    observation_snippet = model.run(control_plan=control_snippet)
+
+    controller.feed(observation_snippet)
     control_plan = controller.plan()
 
-    # run the model
-    pendulum.step(control_plan[0].item())
+    control_snippet = control_plan[:planning_steps]
 
-control_start = controller.history_length
-
-outputs = torch.stack(controller.history.outputs)
+observations = torch.stack(controller.history.observations)
 controls_prenoise = torch.stack(controller.history.controls_prenoise)
 controls = torch.stack(controller.history.controls)
 
@@ -174,7 +140,7 @@ with dv.FigureManager(3, 1, figsize=(8, 6)) as (_, axs):
     axs[0].set_title("Online")
 
     for i, ax in enumerate(axs[:2]):
-        crt_output = outputs[:, i]
+        crt_output = observations[:, i]
 
         yl = (min(crt_output.min(), 0), max(crt_output.max(), 0))
         ax.axhline(0, c="gray", ls=":", lw=1.0)
@@ -185,7 +151,7 @@ with dv.FigureManager(3, 1, figsize=(8, 6)) as (_, axs):
             color="gray",
             alpha=0.5,
             edgecolor="none",
-            label="no control",
+            label="seed",
         )
         ax.plot(crt_output.squeeze(), lw=1.0)
         ax.set_xlabel("time")
@@ -200,7 +166,7 @@ with dv.FigureManager(3, 1, figsize=(8, 6)) as (_, axs):
         color="gray",
         alpha=0.5,
         edgecolor="none",
-        label="no control",
+        label="seed",
     )
     axs[-1].plot(controls.squeeze(), lw=1.0)
     axs[-1].set_xlabel("time")
@@ -210,42 +176,44 @@ with dv.FigureManager(3, 1, figsize=(8, 6)) as (_, axs):
 # %% [markdown]
 # ## Long horizon, online, observe 1 - cosine
 
-# %%
 torch.manual_seed(42)
-control_horizon = 20
-controller = DDController(
-    2,
-    1,
-    seed_length=2,
-    averaging_factor=2.0,
-    control_horizon=control_horizon,
-    noise_handling="none",
-    l2_regularization=5.0,
-    noise_strength=0.01,
-    output_cost=1000.0,
+control_horizon = 12
+controller = DeepControl(
+    control_dim=1,
+    ini_length=2,
+    horizon=control_horizon,
+    l2_regularization=1e-4,
+    control_cost=1e-5,
+    seed_noise_norm=2.0,
     affine=True,
-    # clip_control=(-40, 40),
-    clip_control=(-5, 5),
+    control_norm_clip=5.0,
 )
 
-pendulum = CartPole()
-
-# initial state
-pendulum.q[1] = 1e-6
+model = CartPole(
+    dt=0.02,
+    M=100,
+    observation=lambda state: state[[1, 2]],
+    initial_state=torch.tensor([[0], [np.pi - 0.05], [0], [0]], dtype=torch.float64),
+)
 
 n_steps = 2000
-for k in range(n_steps):
-    # decide on control magnitude
-    obs = torch.hstack((1 - torch.cos(pendulum.q[[1]]), pendulum.dq[[0]] / 100.0))
-    controller.feed(obs)
+planning_steps = 2
+seed_length = 300
+
+control_snippet = controller.generate_seed(seed_length, model.dtype)
+control_start = len(control_snippet)
+for k in range(n_steps // planning_steps):
+    observation_snippet = model.run(control_plan=control_snippet)
+
+    transformed_snippet = observation_snippet.clone()
+    transformed_snippet[:, 0] = 1 - torch.cos(transformed_snippet[:, 0])
+    transformed_snippet[:, 1] /= 100.0
+    controller.feed(observation_snippet)
     control_plan = controller.plan()
 
-    # run the model
-    pendulum.step(control_plan[0].item())
+    control_snippet = control_plan[:planning_steps]
 
-control_start = controller.history_length
-
-outputs = torch.stack(controller.history.outputs)
+observations = torch.stack(controller.history.observations)
 controls_prenoise = torch.stack(controller.history.controls_prenoise)
 controls = torch.stack(controller.history.controls)
 
@@ -254,7 +222,7 @@ with dv.FigureManager(3, 1, figsize=(9, 5)) as (_, axs):
     axs[0].set_title("Online")
 
     for i, ax in enumerate(axs[:2]):
-        crt_output = outputs[:, i]
+        crt_output = observations[:, i]
 
         yl = (min(crt_output.min(), 0), max(crt_output.max(), 0))
         ax.axhline(0, c="gray", ls=":", lw=1.0)
@@ -285,104 +253,5 @@ with dv.FigureManager(3, 1, figsize=(9, 5)) as (_, axs):
     axs[-1].plot(controls.squeeze(), lw=1.0)
     axs[-1].set_xlabel("time")
     axs[-1].set_ylabel("control")
-
-# %% [markdown]
-# ## Long horizon, online, observe 1 - cosine, run more of the control plan at once
-
-# %%
-torch.manual_seed(42)
-control_horizon = 24
-controller = DDController(
-    # 2,
-    1,
-    1,
-    seed_length=2,
-    averaging_factor=4.0,
-    control_horizon=control_horizon,
-    noise_handling="none",
-    l2_regularization=5.0,
-    noise_strength=0.01,
-    output_cost=1000.0,
-    affine=True,
-    # clip_control=(-40, 40),
-    clip_control=(-15, 15),
-)
-
-pendulum = CartPole()
-
-# initial state
-pendulum.q[1] = np.pi - 0.05
-
-n_steps = 2000
-ctrl_stride = 10
-k = 0
-while k < n_steps:
-    if controller.observation_history is None or (
-        len(controller.observation_history) < controller.history_length
-    ):
-        # decide on control magnitude
-        # obs = torch.hstack((1 - torch.cos(pendulum.q[[1]]), pendulum.dq[[0]] / 100.0))
-        obs = 1 - torch.cos(pendulum.q[[1]])
-        controller.feed(obs)
-        control_plan = controller.plan()
-
-        # run the model
-        pendulum.step(control_plan[0].item())
-        k += 1
-    else:
-        for i in range(ctrl_stride):
-            # obs = torch.hstack(
-            #     (1 - torch.cos(pendulum.q[[1]]), pendulum.dq[[0]] / 100.0)
-            # )
-            obs = 1 - torch.cos(pendulum.q[[1]])
-            controller.feed(obs)
-            pendulum.step(control_plan[i].item())
-            k += 1
-
-        control_plan = controller.plan()
-
-control_start = controller.history_length
-
-outputs = torch.stack(controller.history.outputs)
-controls_prenoise = torch.stack(controller.history.controls_prenoise)
-controls = torch.stack(controller.history.controls)
-
-# %%
-with dv.FigureManager(2, 1, figsize=(9, 3)) as (_, axs):
-    axs[0].set_title("Online")
-
-    for i, ax in enumerate(axs[:1]):
-        crt_output = outputs[:, i]
-
-        yl = (min(crt_output.min(), 0), max(crt_output.max(), 0))
-        ax.axhline(0, c="gray", ls=":", lw=1.0)
-        ax.fill_betweenx(
-            yl,
-            [0, 0],
-            2 * [control_start],
-            color="gray",
-            alpha=0.5,
-            edgecolor="none",
-            label="no control",
-        )
-        ax.plot(crt_output.squeeze(), lw=1.0)
-        ax.set_xlabel("time")
-        ax.set_ylabel(["$1 - \\cos(\\theta)$", "$\\dot x$"][i])
-
-    yl = (min(controls.min(), 0), max(controls.max(), 0))
-    axs[-1].axhline(0, c="gray", ls=":", lw=1.0)
-    axs[-1].fill_betweenx(
-        yl,
-        [0, 0],
-        2 * [control_start],
-        color="gray",
-        alpha=0.5,
-        edgecolor="none",
-        label="no control",
-    )
-    axs[-1].plot(controls.squeeze(), lw=1.0)
-    axs[-1].set_xlabel("time")
-    axs[-1].set_ylabel("control")
-
 
 # %%
--- sandbox/experiment_double_inv_pendulum.py
+++ sandbox/experiment_double_inv_pendulum.py
@@ -16,13 +16,13 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pydove as dv
 
-from ddc import DDController
+from ddc import GeneralSystem, DeepControl
 
 # %% [markdown]
 # ## Define the simulators
 
 
-class DoubleCartPole:
+class DoubleCartPole(GeneralSystem):
     def __init__(
         self,
         M: float = 2.0,
@@ -32,6 +32,7 @@ class DoubleCartPole:
         l2: float = 0.4,
         g: float = 9.8,
         dt: float = 0.01,
+        **kwargs,
     ):
         # parameters
         self.M = M
@@ -42,55 +43,66 @@ class DoubleCartPole:
         self.g = g
         self.dt = dt
 
-        # current state: generalized positions and velocities
-        self.q = torch.zeros(3)  # [x, theta_1, theta_2]
-        self.dq = torch.zeros(3)
+        # state: x, theta_1, theta_2, dx, dtheta_1, dtheta_2
+        super().__init__(evolution=self._step, state_dim=6, control_dim=1, **kwargs)
 
-    def step(self, f: float):
+    def _step(self, state: torch.Tensor, control: torch.Tensor) -> torch.Tensor:
         """Apply the given force to the pendulum and update the state by one time step."""
+        assert state.squeeze().shape == (6,)
+        assert control.numel() == 1
+
         Mtot = self.M + self.m1 + self.m2
         m = self.m1 + self.m2
 
-        theta1 = self.q[1]
-        theta2 = self.q[2]
-        c1 = torch.cos(theta1)
-        s1 = torch.sin(theta1)
-        c2 = torch.cos(theta2)
-        s2 = torch.sin(theta2)
+        theta1 = state[1]
+        theta2 = state[2]
+        c1 = torch.cos(theta1).item()
+        s1 = torch.sin(theta1).item()
+        c2 = torch.cos(theta2).item()
+        s2 = torch.sin(theta2).item()
 
         dtheta = theta2 - theta1
-        cdiff = torch.cos(dtheta)
-        sdiff = torch.sin(dtheta)
+        cdiff = torch.cos(dtheta).item()
+        sdiff = torch.sin(dtheta).item()
 
-        dq1sq = self.dq[1] ** 2
-        dq2sq = self.dq[2] ** 2
+        dq1sq = state[4].item() ** 2
+        dq2sq = state[5].item() ** 2
 
+        f = control.item()
+
+        dtype = state.dtype
         A = torch.tensor(
             [
                 [Mtot, self.l1 * m * c1, self.m2 * self.l2 * c2],
                 [m * c1, m * self.l1, self.m2 * self.l2 * cdiff],
                 [c2, self.l1 * cdiff, self.l2],
-            ]
+            ],
+            dtype=dtype,
         )
         b = torch.tensor(
             [
                 [f + m * self.l1 * dq1sq * s1 + self.m2 * self.l2 * dq2sq * s2],
                 [m * self.g * s1 + self.m2 * self.l2 * dq2sq * sdiff],
                 [self.g * s2 - self.l1 * dq1sq * sdiff],
-            ]
+            ],
+            dtype=dtype,
         )
 
         result = torch.linalg.lstsq(A, b)
         ddq = result.solution.squeeze()
 
         # leap frog
-        change = (0.5 * self.dt) * ddq
-        self.dq += change
-        self.q += self.dt * self.dq
-        self.dq += change
+        new_state = state.clone()
+        change = (0.5 * self.dt) * ddq[:, None]
+
+        new_state[3:] += change
+        new_state[:3] += self.dt * new_state[3:]
+        new_state[3:] += change
+
+        return new_state
 
 
-class DoubleInvertedPendulum:
+class DoubleInvertedPendulum(GeneralSystem):
     def __init__(
         self,
         m1: float = 1.0,
@@ -99,6 +111,7 @@ class DoubleInvertedPendulum:
         l2: float = 0.4,
         g: float = 9.8,
         dt: float = 0.01,
+        **kwargs,
     ):
         # parameters
         self.m1 = m1
@@ -108,71 +121,81 @@ class DoubleInvertedPendulum:
         self.g = g
         self.dt = dt
 
-        # current state: generalized positions and velocities
-        self.q = torch.zeros(2)  # [theta_1, theta_2]
-        self.dq = torch.zeros(2)
+        # state: theta_1, theta_2, dtheta_1, dtheta_2
+        super().__init__(evolution=self._step, state_dim=4, control_dim=2, **kwargs)
 
-    def step(self, torques: torch.Tensor):
+    def _step(self, state: torch.Tensor, control: torch.Tensor):
         """Apply the given torques to the pendulums and update the state by one time
         step.
         """
+        assert state.squeeze().shape == (4,)
+        assert control.squeeze().shape == (2,)
+
         m = self.m1 + self.m2
 
-        theta1 = self.q[0]
-        theta2 = self.q[1]
-        c1 = torch.cos(theta1)
-        s1 = torch.sin(theta1)
-        c2 = torch.cos(theta2)
-        s2 = torch.sin(theta2)
+        theta1 = state[0]
+        theta2 = state[1]
+        c1 = torch.cos(theta1).item()
+        s1 = torch.sin(theta1).item()
+        c2 = torch.cos(theta2).item()
+        s2 = torch.sin(theta2).item()
 
         dtheta = theta2 - theta1
-        cdiff = torch.cos(dtheta)
-        sdiff = torch.sin(dtheta)
+        cdiff = torch.cos(dtheta).item()
+        sdiff = torch.sin(dtheta).item()
 
-        dq1sq = self.dq[0] ** 2
-        dq2sq = self.dq[1] ** 2
+        dq1sq = state[2].item() ** 2
+        dq2sq = state[3].item() ** 2
 
+        dtype = state.dtype
         A = torch.tensor(
             [
                 [m * self.l1, self.m2 * self.l2 * cdiff],
                 [self.l1 * cdiff, self.l2],
-            ]
+            ],
+            dtype=dtype,
         )
         b = (
             torch.tensor(
                 [
                     [m * self.g * s1 + self.m2 * self.l2 * dq2sq * sdiff],
                     [self.g * s2 - self.l1 * dq1sq * sdiff],
-                ]
+                ],
+                dtype=dtype,
             )
-            + torques
+            + control
         )
 
         result = torch.linalg.lstsq(A, b)
         ddq = result.solution.squeeze()
 
         # leap frog
-        change = (0.5 * self.dt) * ddq
-        self.dq += change
-        self.q += self.dt * self.dq
-        self.dq += change
+        new_state = state.clone()
+        change = (0.5 * self.dt) * ddq[:, None]
+        new_state[2:] += change
+        new_state[:2] += self.dt * new_state[2:]
+        new_state[2:] += change
+
+        return new_state
 
 
 # %% [markdown]
 # ### Test the simulation of double cart pole
 
 # %%
-pendulum = DoubleCartPole()
-pendulum.q[1] = np.pi - 0.05
-pendulum.q[2] = np.pi + 0.2
+pendulum = DoubleCartPole(
+    observation=lambda _: _,
+    initial_state=torch.tensor(
+        [0.0, np.pi - 0.05, np.pi + 0.2, 0.0, 0.0, 0.0], dtype=torch.float64
+    ),
+)
 
 n_steps = 1000
 phase = torch.zeros((6, n_steps))
 
 for i in range(n_steps):
-    phase[:3, i] = pendulum.q
-    phase[3:, i] = pendulum.dq
-    pendulum.step(0.0)
+    obs = pendulum.run(1)
+    phase[:, i] = obs.squeeze()
 
 # %%
 with dv.FigureManager(3, 1, figsize=(6, 4), constrained_layout=True) as (_, axs):
@@ -184,17 +207,19 @@ with dv.FigureManager(3, 1, figsize=(6, 4), constrained_layout=True) as (_, axs)
 # ### Test the simulation of double inverted pendulum
 
 # %%
-pendulum = DoubleInvertedPendulum()
-pendulum.q[0] = np.pi - 0.05
-pendulum.q[1] = np.pi + 0.2
+pendulum = DoubleInvertedPendulum(
+    observation=lambda _: _,
+    initial_state=torch.tensor(
+        [np.pi - 0.05, np.pi + 0.2, 0.0, 0.0], dtype=torch.float64
+    ),
+)
 
 n_steps = 1000
 phase = torch.zeros((4, n_steps))
 
 for i in range(n_steps):
-    phase[:2, i] = pendulum.q
-    phase[2:, i] = pendulum.dq
-    pendulum.step(torch.tensor([[0.0], [0.0]]))
+    obs = pendulum.run(1)
+    phase[:, i] = obs.squeeze()
 
 # %%
 with dv.FigureManager(2, 1, figsize=(6, 4), constrained_layout=True) as (_, axs):
@@ -206,40 +231,45 @@ with dv.FigureManager(2, 1, figsize=(6, 4), constrained_layout=True) as (_, axs)
 # ## Long horizon, online, double cart pole
 
 # %%
-torch.manual_seed(42)
-control_horizon = 12
-controller = DDController(
-    2,
-    1,
-    seed_length=3,
-    averaging_factor=4.0,
-    control_horizon=control_horizon,
-    noise_handling="none",
-    l2_regularization=5.0,
-    noise_strength=0.01,
-    output_cost=1000.0,
+torch.manual_seed(41)
+control_horizon = 16
+controller = DeepControl(
+    control_dim=1,
+    ini_length=2,
+    horizon=control_horizon,
+    l2_regularization=1e-5,
+    control_cost=1e-4,
+    seed_noise_norm=0.1,
     affine=True,
-    clip_control=(-40, 40),
+    control_norm_clip=2.0,
 )
 
-pendulum = DoubleCartPole(dt=0.01)
+n_steps = 1000
+dt = 0.01
 
 # initial state
-pendulum.q[1] = np.pi - 0.05
-pendulum.q[2] = np.pi + 0.2
+model = DoubleCartPole(
+    dt=dt,
+    observation=lambda state: state[1:3],
+    initial_state=torch.tensor(
+        [0.0, np.pi - 0.05, np.pi + 0.2, 0.0, 0.0, 0.0], dtype=torch.float64
+    ),
+)
 
-n_steps = 2000
-for k in range(n_steps):
-    # decide on control magnitude
-    controller.feed(pendulum.q[1:])
+planning_steps = 5
+seed_length = 120
+
+control_snippet = controller.generate_seed(seed_length, model.dtype)
+control_start = len(control_snippet)
+for k in range(n_steps // planning_steps):
+    observation_snippet = model.run(control_plan=control_snippet)
+
+    controller.feed(observation_snippet)
     control_plan = controller.plan()
 
-    # run the model
-    pendulum.step(control_plan[0].item())
+    control_snippet = control_plan[:planning_steps]
 
-control_start = controller.history_length
-
-outputs = torch.stack(controller.history.outputs)
+observations = torch.stack(controller.history.observations)
 controls_prenoise = torch.stack(controller.history.controls_prenoise)
 controls = torch.stack(controller.history.controls)
 
@@ -248,7 +278,7 @@ with dv.FigureManager(3, 1, figsize=(8, 6)) as (_, axs):
     axs[0].set_title("Online")
 
     for i, ax in enumerate(axs[:2]):
-        crt_output = outputs[:, i]
+        crt_output = observations[:, i]
 
         yl = (min(crt_output.min(), 0), max(crt_output.max(), 0))
         ax.axhline(0, c="gray", ls=":", lw=1.0)
@@ -284,40 +314,45 @@ with dv.FigureManager(3, 1, figsize=(8, 6)) as (_, axs):
 # ## Long horizon, online, double inverted pendulum
 
 # %%
-torch.manual_seed(42)
-control_horizon = 12
-controller = DDController(
-    2,
-    1,
-    seed_length=2,
-    averaging_factor=4.0,
-    control_horizon=control_horizon,
-    noise_handling="none",
-    l2_regularization=20.0,
-    noise_strength=0.01,
-    output_cost=1000.0,
+torch.manual_seed(41)
+control_horizon = 16
+controller = DeepControl(
+    control_dim=2,
+    ini_length=2,
+    horizon=control_horizon,
+    l2_regularization=1e-5,
+    control_cost=1e-4,
+    seed_noise_norm=0.1,
     affine=True,
-    clip_control=(-15, 15),
+    control_norm_clip=2.0,
 )
 
-pendulum = DoubleInvertedPendulum(dt=0.01)
+n_steps = 1000
+dt = 0.01
 
 # initial state
-pendulum.q[0] = np.pi - 0.05
-pendulum.q[1] = np.pi + 0.2
+model = DoubleInvertedPendulum(
+    dt=dt,
+    observation=lambda state: state[:2],
+    initial_state=torch.tensor(
+        [np.pi - 0.05, np.pi + 0.2, 0.0, 0.0], dtype=torch.float64
+    ),
+)
 
-n_steps = 1000
-for k in range(n_steps):
-    # decide on control magnitude
-    controller.feed(pendulum.q)
+planning_steps = 5
+seed_length = 120
+
+control_snippet = controller.generate_seed(seed_length, model.dtype)
+control_start = len(control_snippet)
+for k in range(n_steps // planning_steps):
+    observation_snippet = model.run(control_plan=control_snippet)
+
+    controller.feed(observation_snippet)
     control_plan = controller.plan()
 
-    # run the model
-    pendulum.step(control_plan[0])
+    control_snippet = control_plan[:planning_steps]
 
-control_start = controller.history_length
-
-outputs = torch.stack(controller.history.outputs)
+observations = torch.stack(controller.history.observations)
 controls_prenoise = torch.stack(controller.history.controls_prenoise)
 controls = torch.stack(controller.history.controls)
 
@@ -326,7 +361,7 @@ with dv.FigureManager(3, 1, figsize=(8, 6)) as (_, axs):
     axs[0].set_title("Online")
 
     for i, ax in enumerate(axs[:2]):
-        crt_output = outputs[:, i]
+        crt_output = observations[:, i]
 
         yl = (min(crt_output.min(), 0), max(crt_output.max(), 0))
         ax.axhline(0, c="gray", ls=":", lw=1.0)
@@ -356,6 +391,6 @@ with dv.FigureManager(3, 1, figsize=(8, 6)) as (_, axs):
     )
     axs[-1].plot(controls.squeeze(), lw=1.0)
     axs[-1].set_xlabel("time")
-    axs[-1].set_ylabel("control")
+    axs[-1].set_ylabel("controls")
 
 # %%
