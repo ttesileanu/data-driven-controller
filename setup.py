8a0949ef1d4f07f01b58772a51a6710fdc7c3e33
Tiberiu Tesileanu <ttesileanu@flatironinstitute.org>
2023-09-19 12:07:43 -0400

    Update deps, switch to modern pip setup

--- setup.py
+++ setup.py
@@ -1,14 +1,3 @@
-from distutils.core import setup
+from setuptools import setup
 
-setup(
-    name="ddc",
-    version="0.0.1",
-    author="Tiberiu Tesileanu",
-    author_email="ttesileanu@flatironinstitute.org",
-    url="https://github.com/ttesileanu/bio-ddc",
-    packages=["ddc"],
-    package_dir={"": "src"},
-    install_requires=[
-        "setuptools",
-    ],
-)
+setup()
